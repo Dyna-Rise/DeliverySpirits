e67500f02a50f0bf1676e59311a789fb23ff4785
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-07-02 10:13:28 +0900

    GameOverPanelの作成

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,6 +8,8 @@ public class UIController : MonoBehaviour
     public TimeController timeCnt; //TimeControllerスクリプトの情報
     public TextMeshProUGUI timeText; //TimeTextオブジェクトについているTMPのコンポーネントの情報
 
+    public GameObject gameOverPanel; //ゲームオーバーUIを参照
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,25 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //プレイング状態ならタイムカウントする
         if(GameController.gameState == GameState.playing)
         {
             //切り上げたdisplayTimeをstringに変換してtextに差し替え
             timeText.text = Mathf.Ceil(timeCnt.displayTime).ToString();
+        }
+
+        //ゲームオーバーになったら
+        if (GameController.gameState == GameState.gameover)
+        {
+            //ゲームオーバーパネルを表示
+            gameOverPanel.SetActive(true);
+
+            //カーソルの復活
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            //ゲーム状態をendに
+            GameController.gameState = GameState.end;
         }
     }
 }
